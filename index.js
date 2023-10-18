43260cde7c0d27f18cc766d9ccfd7abc9b610f9b
PrabhjotSingh03 <prabhjot.sains@gmail.com>
2023-10-18 01:40:30 -0400

    Added Stripe

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ const SECRET = "SECRET";
 const jwt = require('jsonwebtoken');
 const { isAuthenticate, userSanitize, cookieExtractor } = require("./services/common");
 const cookieParser = require("cookie-parser");
-
 //from passportjs
 const opts = {}
 opts.jwtFromRequest = cookieExtractor;
 opts.secretOrKey = SECRET;
 
+server.use(express.raw({type:"application/json"}));
 server.use(cookieParser());
 server.use(express.static("build"));
 server.use(
@@ -102,6 +102,57 @@ passport.deserializeUser(function (user, cb) {
   });
 });
 // Till here
+
+//Stripe Payments
+// This is your test secret API key.
+const stripe = require("stripe")('sk_test_51O2QBjKwE7BrlPUUv2Y68634dUO9REP9iaQtWTfd40UfrS0sqXeo6uqHx8b24dNuW5kMbhKG5vN2atn6ouTAET0R0025IaESch');
+
+server.post("/create-payment-intent", async (req, res) => {
+  const { totalAmount } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: totalAmount*100,
+    currency: "cad",
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+//webhook
+const endpointSecret = "whsec_6026a1503e0c9d8ff41ee57ee495fcae8872f715300db8f78b47deed8edc4401";
+
+server.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntentSucceeded = event.data.object;
+      // Then define and call a function to handle the event payment_intent.succeeded
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
 
 main().catch((error) => console.log(error));
 
